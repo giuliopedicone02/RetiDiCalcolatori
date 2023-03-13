a4d7d16282fcd82d0d2cb927d4c17ca196e7b0db
Giulio Pedicone <pediconegiulio02@gmail.com>
2023-03-13 17:27:51 +0100

    Fix

--- Lezioni/2023-03-13/file.c
+++ Lezioni/2023-03-13/file.c
@@ -3,16 +3,21 @@
 int main()
 {
     FILE *file = fopen("input.txt", "r");
+
     if (file == NULL)
     {
         printf("Impossibile aprire il file");
         return 1;
     }
+
     char buffer[100];
-    while (fgets(buffer, 100, file) != NULL)
+
+    while (!feof(file))
     {
-        printf("%s", buffer);
+        fscanf(file, "%s", buffer);
+        printf("%s ", buffer);
     }
+
     fclose(file);
     return 0;
 }
