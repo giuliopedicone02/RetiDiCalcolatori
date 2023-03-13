a7dd2808a30e8d0a6ef207063d3b1fbceaf66c0f
Giulio Pedicone <pediconegiulio02@gmail.com>
2023-03-13 15:33:40 +0100

    Print and Scan variables in C

---
+++ Lezioni/2023-03-13/printVariables.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 10, c = 15;
+    printf("a: %d, b: %d, c: %d", a, b, c);
+}
---
+++ Lezioni/2023-03-13/scanVariables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    printf("Scrivi valore di a: ");
+    scanf("%d", &a);
+
+    printf("Valore inserito: %d", a);
+}
