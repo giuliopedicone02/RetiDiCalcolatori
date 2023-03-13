44c0eba961b07f33236c8da5bab1e94bf1b33c99
Giulio Pedicone <pediconegiulio02@gmail.com>
2023-03-13 17:21:11 +0100

    Gestione file

---
+++ Lezioni/2023-03-13/file.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *file = fopen("input.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("Impossibile aprire il file");
+        return 1;
+    }
+
+    char buffer[100];
+
+    while (!feof(file))
+    {
+        fscanf(file, "%s", buffer);
+        printf("%s", buffer);
+    }
+
+    fclose(file);
+    return 0;
+}
